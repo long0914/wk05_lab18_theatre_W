a31df4e1063f81edc34e31e025a9778ee29fe89d
long0914 <119887736+long0914@users.noreply.github.com>
2024-02-15 19:14:53 +0000

    Add enum time genre

---
+++ DayEnum.cs
@@ -0,0 +1,10 @@
+public enum DayEnum
+{
+    Mon,  // Monday
+    Tue,  // Tuesday
+    Wed,  // Wednesday
+    Thu,  // Thursday
+    Fri,  // Friday
+    Sat,  // Saturday
+    Sun   // Sunday
+}
---
+++ GenreEnum.cs
@@ -0,0 +1,15 @@
+[Flags]
+public enum Genre
+{
+    Unrated = 1,
+    Action = 2,
+    Comedy = 4,
+    Horror = 8,
+    Fantasy = 16,
+    Musical = 32,
+    Mystery = 64,
+    Romance = 128,
+    Adventure = 256,
+    Animation = 512,
+    Documentary = 1024
+}
